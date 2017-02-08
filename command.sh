a5d943c285519a772e2b1725929a7a37def23ddb
sheepolata <antoine.gaget@gmail.com>
2017-02-08 22:10:33 +0100

    Update command.sh

--- command.sh
+++ command.sh
@@ -23,38 +23,44 @@ else
     it=100
 fi
 
-k=3
-m=10000000
+#k=3
+#m=1000
 
 truncate -s 0 "plan_expe/all_data.csv"
 
 
 for name in "madame-bovary" "zadig" "don-quixote"
 do
-	rawtime=$name'_rawtime.txt'
-	countersfile=$exp$name'_counters.txt'
-	nbwordsraw=$exp$name'_nb_words_raw.txt'
-	nbwordsprocessed=$exp$name'_nb_words.txt'
-	datafile=$exp$name'_param.txt'
-
-	truncate -s 0 $rawtime
-	truncate -s 0 $countersfile
-	truncate -s 0 $nbwordsraw
-
-	echo "count1,count2,coutn3" &>> $countersfile
-	echo "k,m" &>> $datafile
-
-	for i in $(seq "$it")
+	for k in 2, 3, 4, 5, 6, 7
 	do
-	   	echo "Iteration $i"
-		(time ./markov_counters $k $m $countersfile <$name'.txt' >$res_algo) &>> $rawtime
-		wc -w $name &>> $nbwordsraw
-		echo "$k,$m" &>> $datafile
-	done
+		for m in 1000, 10000, 100000, 500000
+		do
+			rawtime=$name'_rawtime.txt'
+			countersfile=$exp$name'_counters.txt'
+			nbwordsraw=$exp$name'_nb_words_raw.txt'
+			nbwordsprocessed=$exp$name'_nb_words.txt'
+			datafile=$exp$name'_param.txt'
 
-	python script_count.py $rawtime $exp$name'_timetable.txt' $nbwordsraw $nbwordsprocessed
-	rm $rawtime
-	rm $nbwordsraw
+			truncate -s 0 $rawtime
+			truncate -s 0 $countersfile
+			truncate -s 0 $nbwordsraw
+
+			echo "count1,count2,coutn3" &>> $countersfile
+			echo "k,m" &>> $datafile
+
+			for i in $(seq "$it")
+			do
+				echo "Iteration $i"
+				(time ./markov_counters $k $m $countersfile <$name'.txt' >$res_algo) &>> $rawtime
+				wc -w $name &>> $nbwordsraw
+				echo "$k,$m" &>> $datafile
+			done
+
+			python script_count.py $rawtime $exp$name'_timetable.txt' $nbwordsraw $nbwordsprocessed
+			rm $rawtime
+			rm $nbwordsraw
+		done
+	done
 done
 
 tmp="*.txt"
